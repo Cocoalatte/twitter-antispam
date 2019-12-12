a3d3cd788d0bb525477fe309880e7cbb0b2162d0
ここあらて <latte-hana+github@chitose-tech.com>
2019-12-12 14:54:19 +0900

    うんちしｔれ

---
+++ unchi.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+require 'pp'
+
+twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ""
+    config.consumer_secret     = ""
+    config.access_token        = ""
+    config.access_token_secret = ""
+end
+cnt = 0
+twitter.follower_ids(twitter.users(ARGV[0])).each do |uids|
+    twitter.block(uids)
+    print uids
+    puts "Blocked:".concat(uids)
+    cnt = cnt + 1
+end
